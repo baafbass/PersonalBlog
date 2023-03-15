e3cc04e23593e07da2a1277fdb3050c620a2e609
baafbass <98693906+baafbass@users.noreply.github.com>
2023-03-15 20:44:38 +0300

    For the all tables's common column Classes were created and the home page sliders's table's class is created too

---
+++ PersonalBlog.Entities/Concrete/HomePageSliders.cs
@@ -0,0 +1,16 @@
+﻿using PersonalBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalBlog.Entities.Concrete
+{
+    public class HomePageSliders : EntityBase,IEntity
+    {
+        public string Title { get; set; }
+        public string ShortContent { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ PersonalBlog.Shared/Entities/Abstract/EntityBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalBlog.Shared.Entities.Abstract
+{
+    public abstract class EntityBase
+    {
+        public virtual int ID { get; set; }
+        public virtual DateTime CreatedTime { get; set; } = DateTime.Now;
+        public virtual DateTime ModifiedTime { get; set; } = DateTime.Now;
+        public virtual bool IsActive { get; set; } = false;
+        public virtual bool IsDeleted { get; set; } = false;
+        public virtual string CreatedByName { get; set; } = "Admin";
+        public virtual string ModifiedByName { get; set; } = "Admin";
+    }
+}
