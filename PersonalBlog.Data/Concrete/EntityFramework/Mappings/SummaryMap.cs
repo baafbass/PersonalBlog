e9201e68298db724da6cce7ca4e8b8262f778c6d
baafbass <98693906+baafbass@users.noreply.github.com>
2023-03-18 19:36:56 +0300

    Starting Mapping operations with Fluent API

---
+++ PersonalBlog.Data/Concrete/EntityFramework/Mappings/SummaryMap.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PersonalBlog.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalBlog.Data.Concrete.EntityFramework.Mappings
+{
+    public class SummaryMap : IEntityTypeConfiguration<Summary>
+    {
+        public void Configure(EntityTypeBuilder<Summary> builder)
+        {
+            builder.HasKey(x => x.ID);
+            builder.Property(x => x.ID).ValueGeneratedOnAdd();
+            builder.Property(x => x.CreatedByName).HasMaxLength(50);
+            builder.Property(x => x.CreatedByName).IsRequired();
+            builder.Property(x => x.ModifiedByName).HasMaxLength(50);
+            builder.Property(x => x.ModifiedByName).IsRequired();
+            builder.Property(x => x.IsActive).IsRequired();
+            builder.Property(x => x.IsDeleted).IsRequired();
+            builder.Property(x => x.Content).IsRequired();
+            builder.Property(x => x.Content).HasColumnType("NVARCHAR(MAX)");
+            builder.ToTable("Summary");
+            builder.HasData(new Summary
+            {
+                ID = 1,
+                CreatedByName = "InitialCreated",
+                ModifiedByName = "InitialCreated",
+                CreatedTime = DateTime.Now,
+                ModifiedTime = DateTime.Now,
+                IsActive = false,
+                IsDeleted = false,
+                Content = "Welcome to my blog! I am a second-year Information System Engineering student who is passionate about software development. Currently, I am working on improving my skills in C# and .Net technologies by building projects. I also have experience with HTML, CSS, and JavaScript and have completed many projects using C and C++ languages. As a big fan of cybersecurity, I am very attentive to security during the software development process."
+            });
+        }
+    }
+}
